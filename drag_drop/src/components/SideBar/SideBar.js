7a818a395751739c4aa12fa7bd5ac197b731a0c7
karanhere3962 <karanhere3962@gmail.com>
2020-06-24 00:04:42 +0530

    complete

--- drag_drop/src/components/SideBar/SideBar.js
+++ drag_drop/src/components/SideBar/SideBar.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./SideBar.css";
-import Card from "../Card/Card";
 import { randomStringGenerator } from "../../helperFunctions";
 import SidebarComponent from "../SidebarComponent/SidebarComponent";
+import stateHolder from "../../stateHolder";
 
 const SideBar = (props) => {
   return (
     <div className="sideBar">
-      <SidebarComponent id={"sideBarComponent" + randomStringGenerator()} />
+      <SidebarComponent
+        id={"sideBarComponent" + randomStringGenerator()}
+        shouldBeDraggable={true}
+      />
+      <SidebarComponent
+        id={"sideBarComponent" + randomStringGenerator()}
+        shouldBeDraggable={false}
+        content="Download JSON"
+        onClick={() => {
+          let componentState = JSON.stringify(
+            stateHolder.returnComponentJson()
+          );
+          const element = document.createElement("a");
+          const file = new Blob([componentState], { type: "text/plain" });
+          element.href = URL.createObjectURL(file);
+          element.download = "componentStates.json";
+          document.body.appendChild(element);
+          element.click();
+        }}
+      />
     </div>
   );
 };
--- drag_drop/src/components/SidebarComponent/SidebarComponent.js
+++ drag_drop/src/components/SidebarComponent/SidebarComponent.js
@@ -4,21 +4,28 @@ import ItemTypes from "../../ItemTypes";
 import "./SidebarComponent.css";
 const SidebarComponent = (props) => {
   const id = props.id ? props.id : "cardCreator";
-
   const [{ isDragging }, drag] = useDrag({
     item: { type: ItemTypes.CARDCREATOR, id: id },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
-  return (
-    <div
-      ref={drag}
-      className={isDragging ? "cardCreator dragging" : "cardCreator"}
-    >
-      {props.content ? props.content : "Drag and Drop on Canvas"}
-    </div>
-  );
+  if (props.shouldBeDraggable) {
+    return (
+      <div
+        ref={drag}
+        className={isDragging ? "cardCreator dragging" : "cardCreator"}
+      >
+        {props.content ? props.content : "Drag and Drop on Canvas"}
+      </div>
+    );
+  } else {
+    return (
+      <div className={"cardCreator"} onClick={props.onClick}>
+        {props.content ? props.content : "Drag and Drop on Canvas"}
+      </div>
+    );
+  }
 };
 
 export default SidebarComponent;
